2e2e7e26e059d94e2e535b104237d4ad5552c900
miltonjohnston <miltonjohnston14226@gmail.com>
2023-07-05 01:24:26 +0100

    Working on the main page...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
-function App() {
+export default function App() {
+  const [thirdPerson, setThirdPerson] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
+  useEffect(() => {
+    function keydownHandler(e) {
+      if (e.key == "k") {
+        // random is necessary to trigger a state change
+        if (thirdPerson) setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        setThirdPerson(!thirdPerson);
+      }
+    }
+
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+  }, [thirdPerson]);
+
   return (
-    <div>App</div>
+    <Suspense fallback={null}>
+      <Environment
+        background={"both"}
+        files={'/src/Assets/Textures/envmap.hdr'} />
+
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {!thirdPerson && (
+        <OrbitControls target={[-2.64, -0.71, 0.03]} />
+      )}
+    </Suspense>
   )
 }
-
-export default App
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Canvas } from "@react-three/fiber";
 
 import App from './App.jsx'
 import "./Styles/Index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <App />
+    </Canvas>
   </React.StrictMode>,
 )
